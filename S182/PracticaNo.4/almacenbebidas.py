988cde2c13053daaf429046b6cd86988feff74f7
ricardofm09 <133308075+ricardofm09@users.noreply.github.com>
2023-06-02 18:23:46 -0600

    AlmacenBebidas_Completa

--- S182/PracticaNo.4/almacenbebidas.py
+++ S182/PracticaNo.4/almacenbebidas.py
@@ -1,78 +1,143 @@
 class Bebida:
-    def __init__(self, id, nombre, precio, marca, clasificacion):
+    def __init__(self, id, nombre, precio, clasificacion, marca):
         self.id = id
         self.nombre = nombre
         self.precio = precio
-        self.marca = marca
         self.clasificacion = clasificacion
-
+        self.marca = marca
 
 class AlmacenBebidas:
     def __init__(self):
         self.bebidas = []
 
-    def alta_bebida(self, bebida):
+    def dar_de_alta(self, bebida):
         self.bebidas.append(bebida)
+        print("Bebida agregada exitosamente.")
 
-    def baja_bebida(self, id):
-        self.bebidas = [bebida for bebida in self.bebidas if bebida.id != id]
+    def dar_de_baja(self, id):
+        for bebida in self.bebidas:
+            if bebida.id == id:
+                self.bebidas.remove(bebida)
+                print("Bebida eliminada exitosamente.")
+                return
+        print("No se encontró una bebida con el ID proporcionado.")
 
-    def actualizar_bebida(self, id, nombre, clasificacion, marca, precio):
+    def actualizar_producto(self, id, nombre, precio, clasificacion, marca):
         for bebida in self.bebidas:
             if bebida.id == id:
                 bebida.nombre = nombre
+                bebida.precio = precio
                 bebida.clasificacion = clasificacion
                 bebida.marca = marca
-                bebida.precio = precio
-                break
+                print("Bebida actualizada exitosamente.")
+                return
+        print("No se encontró una bebida con el ID proporcionado.")
 
     def mostrar_bebidas(self):
+        if len(self.bebidas) == 0:
+            print("No hay bebidas en el almacén.")
+        else:
+            for bebida in self.bebidas:
+                print("ID:", bebida.id)
+                print("Nombre:", bebida.nombre)
+                print("Precio:", bebida.precio)
+                print("Clasificación:", bebida.clasificacion)
+                print("Marca:", bebida.marca)
+                print()
+
+    def calcular_precio_promedio(self):
+        if len(self.bebidas) == 0:
+            print("No hay bebidas en el almacén.")
+        else:
+            total = 0
+            for bebida in self.bebidas:
+                total += bebida.precio
+            precio_promedio = total / len(self.bebidas)
+            print("El precio promedio de las bebidas es:", precio_promedio)
+
+    def contar_bebidas_por_marca(self, marca):
+        cantidad = 0
         for bebida in self.bebidas:
-            print(f"ID: {bebida.id}")
-            print(f"Nombre: {bebida.nombre}")
-            print(f"Clasificación: {bebida.clasificacion}")
-            print(f"Marca: {bebida.marca}")
-            print(f"Precio: {bebida.precio}")
-            print("---------------------------")
+            if bebida.marca == marca:
+                cantidad += 1
+        print("Cantidad de bebidas de la marca", marca, ":", cantidad)
 
-    def precio_promedio_bebidas(self):
-        total_precios = sum(bebida.precio for bebida in self.bebidas)
-        promedio = total_precios / len(self.bebidas)
-        return promedio
+    def contar_bebidas_por_clasificacion(self, clasificacion):
+        cantidad = 0
+        for bebida in self.bebidas:
+            if bebida.clasificacion == clasificacion:
+                cantidad += 1
+        print("Cantidad de bebidas de la clasificación", clasificacion, ":", cantidad)
 
-    def cantidad_bebidas_marca(self, marca):
-        cantidad = sum(1 for bebida in self.bebidas if bebida.marca == marca)
-        return cantidad
+# Función para mostrar el menú y obtener la opción seleccionada
+def mostrar_menu():
+    print("------ Menú ------")
+    print("1. Dar de Alta una bebida")
+    print("2. Dar de Baja una bebida")
+    print("3. Actualizar una bebida")
+    print("4. Mostrar todas las bebidas")
+    print("5. Calcular precio promedio de las bebidas")
+    print("6. Contar bebidas de una marca")
+    print("7. Contar bebidas por clasificación")
+    print("8. Salir")
+    print()
 
-    def cantidad_por_clasificacion(self, clasificacion):
-        cantidad = sum(1 for bebida in self.bebidas if bebida.clasificacion == clasificacion)
-        return cantidad
+    opcion = input("Selecciona una opción: ")
+    return opcion
 
-
-# Ejemplo de uso
+# Almacén de bebidas
 almacen = AlmacenBebidas()
 
-bebida1 = Bebida(1, "Agua Mineral", "Agua", "MarcaA", 1.5)
-bebida2 = Bebida(2, "Refresco de Cola", "Bebida Azucarada", "MarcaB", 2.0)
-bebida3 = Bebida(3, "Bebida Energética", "Bebida Energética", "MarcaA", 3.5)
+# Ciclo principal
+while True:
+    opcion = mostrar_menu()
 
-almacen.alta_bebida(bebida1)
-almacen.alta_bebida(bebida2)
-almacen.alta_bebida(bebida3)
+    if opcion == "1":
+        id = input("ID de la bebida: ")
+        nombre = input("Nombre de la bebida: ")
+        precio = float(input("Precio de la bebida: "))
+        clasificacion = input("Clasificación de la bebida: ")
+        marca = input("Marca de la bebida: ")
+        bebida = Bebida(id, nombre, precio, clasificacion, marca)
+        almacen.dar_de_alta(bebida)
+        print()
 
-almacen.mostrar_bebidas()
+    elif opcion == "2":
+        id = input("ID de la bebida a eliminar: ")
+        almacen.dar_de_baja(id)
+        print()
 
-almacen.baja_bebida(2)
+    elif opcion == "3":
+        id = input("ID de la bebida a actualizar: ")
+        nombre = input("Nuevo nombre de la bebida: ")
+        precio = float(input("Nuevo precio de la bebida: "))
+        clasificacion = input("Nueva clasificación de la bebida: ")
+        marca = input("Nueva marca de la bebida: ")
+        almacen.actualizar_producto(id, nombre, precio, clasificacion, marca)
+        print()
 
-almacen.actualizar_bebida(1, "Agua Mineral con Gas", "Agua", "MarcaA", 2.0)
+    elif opcion == "4":
+        almacen.mostrar_bebidas()
+        print()
 
-almacen.mostrar_bebidas()
+    elif opcion == "5":
+        almacen.calcular_precio_promedio()
+        print()
 
-promedio = almacen.precio_promedio_bebidas()
-print(f"Precio promedio de bebidas: {promedio}")
+    elif opcion == "6":
+        marca = input("Marca de las bebidas a contar: ")
+        almacen.contar_bebidas_por_marca(marca)
+        print()
 
-cantidad_marca = almacen.cantidad_bebidas_marca("MarcaA")
-print(f"Cantidad de bebidas de MarcaA: {cantidad_marca}")
+    elif opcion == "7":
+        clasificacion = input("Clasificación de las bebidas a contar: ")
+        almacen.contar_bebidas_por_clasificacion(clasificacion)
+        print()
 
-cantidad_clasificacion = almacen.cantidad_por_clasificacion("Bebida Energética")
-print(f"Cantidad de bebidas de clasificación 'Bebida Energética': {cantidad_clasificacion}")
+    elif opcion == "8":
+        print("Hasta Pronto")
+        break
+
+    else:
+        print("Opción inválida. Por favor, selecciona una opción válida.")
+        print()
