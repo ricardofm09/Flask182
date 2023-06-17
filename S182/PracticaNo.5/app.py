dc822128717e9bbc16b4edf904e20bc1c68beb5f
ricardofm09 <133308075+ricardofm09@users.noreply.github.com>
2023-06-16 18:49:09 -0600

    pract6:Vistas, Envio informacion de vista a rutas

--- S182/PracticaNo.5/app.py
+++ S182/PracticaNo.5/app.py
@@ -1,22 +1,37 @@
-from flask import Flask 
+
+from flask import Flask,render_template,request
 
 
 #inicializacion del servido Flask 
 app= Flask(__name__)
+
+#configurarciones de conexion a BD
+app.config['MYSQL_HOST']= "localhost"
+app.config['MYSQL_USER']= "root"
+app.config['MYSQL_PASSWORD']= "123456"
+app.config['MYSQL_BD']= "bdflask"
+
+
 
 #Declaracion de rutas
 
 
 # ruta se compone del nombre y funcion
 
-# Ruta Index http://localhost:5000
+# ruta Index http://localhost:5000
 @app.route('/')
 def index():
-    return "Hola Mundo"
+    return render_template('index.html')
 
-@app.route('/guardar')
+@app.route('/guardar',methods=['POST'])
 def guardar():
-    return "se guardo el album en la BD"
+    if request.method == 'POST':
+        titulo = request.form['txtTitulo']
+        artista = request.form['txtArtista']
+        anio = request.form['txtAnio']
+        print(titulo,artista,anio)
+        
+    return "La info del Album llego a su ruta Amigo :)"
 
 @app.route('/eliminar')
 def eliminar():
@@ -24,4 +39,4 @@ def eliminar():
 
 # Ejecucion de servidor 
 if __name__==  'main_':
-        app.run(port= 5000)
+        app.run(port= 5000,debug=True)
