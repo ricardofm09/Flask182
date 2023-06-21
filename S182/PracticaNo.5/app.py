8f9e237875e00c1ebda7059a66ae7a8c1a4196f6
ricardofm09 <133308075+ricardofm09@users.noreply.github.com>
2023-06-20 19:03:58 -0600

    Entrega de la bd y html

--- S182/PracticaNo.5/app.py
+++ S182/PracticaNo.5/app.py
@@ -1,5 +1,6 @@
 
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request, redirect, url_for, flash
+from flask_mysqldb import MySQL
 
 
 #inicializacion del servido Flask 
@@ -8,10 +9,12 @@ app= Flask(__name__)
 #configurarciones de conexion a BD
 app.config['MYSQL_HOST']= "localhost"
 app.config['MYSQL_USER']= "root"
-app.config['MYSQL_PASSWORD']= "123456"
-app.config['MYSQL_BD']= "bdflask"
+app.config['MYSQL_PASSWORD']= ""
+app.config['MYSQL_DB']= "dbflask"
 
+app.secret_key='mysecretkey'
 
+mysql= MySQL(app)
 
 #Declaracion de rutas
 
@@ -23,20 +26,26 @@ app.config['MYSQL_BD']= "bdflask"
 def index():
     return render_template('index.html')
 
-@app.route('/guardar',methods=['POST'])
+@app.route('/guardar', methods=['POST'])
 def guardar():
     if request.method == 'POST':
-        titulo = request.form['txtTitulo']
-        artista = request.form['txtArtista']
-        anio = request.form['txtAnio']
-        print(titulo,artista,anio)
+        Vtitulo = request.form['txtTitulo']
+        Vartista = request.form['txtArtista']
+        Vanio = request.form['txtAnio']
         
-    return "La info del Album llego a su ruta Amigo :)"
+        # Conectamos y ejecutamos el insert
+        CS= mysql.connection.cursor()
+        CS.execute('insert into albums(titulo,artista,anio) values(%s,%s,%s)',(Vtitulo,Vartista,Vanio))
+        mysql.connection.commit()
+
+
+    flash('Album agregado correctamente') 
+    return redirect(url_for('index'))
 
 @app.route('/eliminar')
 def eliminar():
     return "se elimino el album en la BD"
 
 # Ejecucion de servidor 
-if __name__==  'main_':
+if __name__==  '__main__':
         app.run(port= 5000,debug=True)
